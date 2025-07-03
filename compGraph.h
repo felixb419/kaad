e5e6a4498df26d678ae0d49a1dc05905bfe9d387
felixb419 <felix.bertl@fh-krems.ac.at>
2025-07-03 22:49:23 +0200

    refactored and removed unused functions

--- compGraph.h
+++ compGraph.h
@@ -13,13 +13,12 @@ struct CompGraph {
         return raw;
     } 
 
-    // evaluate tensors in CompGraph at index specified
-    // returns reference array to Tensors in given order
+    // evaluate nodes specified by node_ptrs and output array of values in order
     template<typename... ptrs>
-    array<Tensor<T>*, sizeof...(ptrs)> evaluate(ptrs... nodes) {
-        INode<T>* node_arr[] = {nodes...};
-        array<Tensor<T>*, sizeof...(nodes)> values;
-        for (int i = 0; i < sizeof...(nodes); i++) {
+    array<Tensor<T>*, sizeof...(ptrs)> evaluate(ptrs... node_ptrs) {
+        INode<T>* node_arr[] = {node_ptrs...};
+        array<Tensor<T>*, sizeof...(node_ptrs)> values;
+        for (int i = 0; i < sizeof...(node_ptrs); i++) {
             node_arr[i]->eval();
             values[i] = &(node_arr[i]->value);
         }
@@ -27,18 +26,17 @@ struct CompGraph {
         return values;
     }
 
-    // calculates partial derivatives for all Node going back from f
-    // returns reference to derivative of f w.r.t. x
+    // compute gradients of Nodes in graph w.r.t. df and output array of gradients of dx
     template<typename... ptrs>
-    array<Tensor<T>*, sizeof...(ptrs)> getGradient(INode<T>* df_idx, ptrs... dx_idx) {
-        INode<T>& f = *df_idx;
+    array<Tensor<T>*, sizeof...(ptrs)> getGradient(INode<T>* df_ptr, ptrs... dx_ptrs) {
+        INode<T>& f = *df_ptr;
         fill(f.gradient.val, f.gradient.val + f.gradient.len, 1.0);
     
         f.getGrad();
     
-        INode<T>* node_arr[] = {dx_idx...};
-        array<Tensor<T>*, sizeof...(dx_idx)> partials;
-        for (size_t i = 0; i < sizeof...(dx_idx); i++) {
+        INode<T>* node_arr[] = {dx_ptrs...};
+        array<Tensor<T>*, sizeof...(dx_ptrs)> partials;
+        for (size_t i = 0; i < sizeof...(dx_ptrs); i++) {
             partials[i] = &(node_arr[i]->gradient);
         }
 
@@ -48,42 +46,6 @@ struct CompGraph {
     void reset() {
         for (INode<T>* node : nodes) {
             node->reset();
-        }
-    }
-
-    private:
-    tView<T> eval(int index) {
-        INode<T>& node = *nodes[index];
-        if (node.evaluated) return node.value.view();
-    
-        tView<T> val1 = this->eval(node.in1);
-        tView<T> val2 = node.in2 < 0 ? tView<T>() : this->eval(node.in2);
-        tView<T> out = node.value.view();
-    
-        node.eval(val1.val, val2.val, node.value.val);
-    
-        return out;
-    }
-
-    void grad(int index) {
-        INode<T>& node = *nodes[index];
-
-        tView<T> value = node.value.view();
-        tView<T> seed = node.gradient.view();
-    
-        tView<T> val1 = nodes[node.in1].value.view();
-        tView<T> grad1 = nodes[node.in1].gradient.view();
-        int in2 = node.in2;
-        tView<T> val2 = in2 < 0 ? tView<T>() : nodes[in2].value.view();
-        tView<T> grad2 = in2 < 0 ? tView<T>() : nodes[in2].gradient.view();
-    
-        node.grad(nodes[node.in1].value.val, nodes[node.in2].value.val, node.value.val, nodes[node.in1].gradient.val, nodes[node.in2].gradient.val, node.gradient.val);
-    
-        if (nodes[node.in1].hasInputs) {
-            grad(node.in1);
-        }
-        if (nodes[in2].hasInputs && in2 >= 0) {
-            grad(in2);
         }
     }
 };
