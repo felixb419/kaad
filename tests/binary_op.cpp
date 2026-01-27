76d98c66a5c820d453d7ba0b5aa62ef42bddbb69
FelixBertl <felix.bertl@fh-krems.ac.at>
2026-01-27 15:56:57 +0100

    chore: added binary operation test program

---
+++ tests/binary_op.cpp
@@ -0,0 +1,28 @@
+#include "../kaad.hpp"
+#include <iostream>
+
+int main() {
+    kaad::Computation_graph<float> rec;
+
+    auto a = rec.append(std::vector<int>{3, 2}, 10);
+    auto b = rec.append(std::vector<int>{3, 2}, 30);
+    auto c = rec.append(std::vector<int>{1}, 50);
+    auto d = rec.append(std::vector<int>{2, 3, 1}, 20);
+
+    auto ab = add(rec, a, b);
+    auto abc = add(rec, ab, c);
+    auto res = add(rec, abc, d);
+
+    rec.reset();
+
+    rec.evaluate(res);
+
+    rec.getGradient(res, a, b, d, res);
+
+    std::cout << "A:\n" << a << std::endl;
+    std::cout << "B:\n" << b << std::endl;
+    std::cout << "D:\n" << d << std::endl;
+    std::cout << "Res:\n" << res << std::endl;
+
+    return 0;
+}
