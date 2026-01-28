f05aaecc12ba5ca8f57001d2f8b0b6d9ae3625ad
FelixBertl <felix.bertl@fh-krems.ac.at>
2026-01-28 08:18:54 +0100

    refactor(computation_graph): moved combine_flexible into binary_operators

--- computation_graph/operators/binary_operators.hpp
+++ computation_graph/operators/binary_operators.hpp
@@ -19,6 +19,39 @@ template <typename T, class Kernel> class Node_binary_flex;
 namespace detail {
 
 /**
+ * @brief Computes the resulting shape from broadcasting two tensors.
+ * The broadcasting rules are:
+ *   - dimensions must match
+ *   - or one of them must be 1
+ * @param shape1 Shape of first tensor.
+ * @param nDims1 Number of dimensions of first tensor.
+ * @param shape2 Shape of second tensor.
+ * @param nDims2 Number of dimensions of second tensor.
+ * @param newShape Output array to hold the result shape.
+ * @param newLen Total number of dimensions in the result.
+ * @return true if broadcasting is possible, false otherwise.
+ */
+static inline bool combine_flexible(const int *shape1, size_t nDims1,
+                                    const int *shape2, size_t nDims2,
+                                    int *newShape, size_t newLen) {
+    int ind = newLen - 1;
+    for (int i = 1; i <= newLen; i++, ind--) {
+        int ind1 = nDims1 - i;
+        int ind2 = nDims2 - i;
+        if (ind1 >= 0 && ind2 >= 0) {
+            if (shape1[ind1] != shape2[ind2] && shape1[ind1] != 1 &&
+                shape2[ind2] != 1) {
+                return false;
+            }
+            newShape[ind] = std::max(shape1[ind1], shape2[ind2]);
+        } else {
+            newShape[ind] = ind1 >= 0 ? shape1[ind1] : shape2[ind2];
+        }
+    }
+    return true;
+}
+
+/**
  * @brief Contains a collection of binary functions for multiple versions
  * (sclalarRhs, scalarLhs, pointwise, flexible) of the operation and gradient of
  * a given binary Kernel.
