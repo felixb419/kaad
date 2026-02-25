dce7d90ce7fb340d25a30ea0cf62145f5cb7fc81
FelixBertl <felix.bertl@fh-krems.ac.at>
2026-02-25 22:02:33 +0100

    feat: script to build

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e # exit on first error
+
+# Create build directory
+mkdir -p build
+cd build || exit 1 # exit if cd fails
+
+# Configure CMake and set build type
+cmake -DCMAKE_BUILD_TYPE=Release ..
+
+# Build everything
+cmake --build .
+
+#ctest --output-on-failure
