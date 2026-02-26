6e59452bef0d5238556431f35f3e39f82b031118
FelixBertl <felix.bertl@fh-krems.ac.at>
2026-02-26 08:25:22 +0100

    chore: added examples

---
+++ examples/dot_product.cpp
@@ -0,0 +1,37 @@
+#include "../include/kaad/kaad.hpp"
+#include <algorithm> // for fill
+#include <array>     // for array
+#include <iostream>  // for operator<<
+#include <span>      // for span
+
+int main() {
+    kaad::Computation_graph rec;
+
+    std::span<float> a_vals;
+    auto a = rec.add_input_node(std::array{5}, a_vals);
+    std::fill(a_vals.begin(), a_vals.end(), 10);
+
+    std::span<float> b_vals;
+    auto b = rec.add_input_node(std::array{5}, b_vals);
+    std::fill(b_vals.begin(), b_vals.end(), 50);
+
+    std::span<float> c_vals;
+    auto c = rec.add_input_node(std::array{5}, c_vals);
+    std::fill(c_vals.begin(), c_vals.end(), 20);
+
+    kaad::Node_handle ab = dot(rec, a, b);
+    kaad::Node_handle res = dot(rec, ab, c);
+
+    rec.reset();
+
+    auto e = rec.evaluate(res);
+
+    auto g = rec.getGradient(res, a, b, c);
+
+    std::cout << "A:\n" << a << std::endl;
+    std::cout << "B:\n" << b << std::endl;
+    std::cout << "C:\n" << c << std::endl;
+    std::cout << "Res:\n" << res << std::endl;
+
+    return 0;
+}
---
+++ examples/transpose.cpp
@@ -0,0 +1,26 @@
+#include "../include/kaad/kaad.hpp"
+#include <array>    // for array
+#include <iostream> // for operator<<
+#include <numeric>  // for iota
+#include <span>     // for span
+
+int main() {
+    kaad::Computation_graph rec;
+
+    std::span<float> a_vals;
+    kaad::Node_handle a = rec.add_input_node(std::array{3, 5, 2}, a_vals);
+    std::iota(a_vals.begin(), a_vals.end(), 0);
+
+    kaad::Node_handle c = transpose(rec, a);
+
+    rec.reset();
+
+    auto e = rec.evaluate(c);
+
+    auto g = rec.getGradient(c, a);
+
+    std::cout << "A:\n" << a << std::endl;
+    std::cout << "C:\n" << c << std::endl;
+
+    return 0;
+}
