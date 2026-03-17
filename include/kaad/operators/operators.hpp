cd1d3d1e4622e720bf04eeee8e0e540e5bfec62c
FelixBertl <felix.bertl@fh-krems.ac.at>
2026-03-17 14:19:35 +0100

    doc(operators): adjusted some comments

--- include/kaad/operators/operators.hpp
+++ include/kaad/operators/operators.hpp
@@ -86,8 +86,8 @@ Node abs(Graph &rec, Node input);
  * @brief Adds a slice node to the computation graph.
  * @ingroup unary_operators
  *
- * Extracts a slice from the input tensor node `input`, starting at the
- * specified `offset` and extending for the given `size` along each dimension.
+ * Extracts a slice from the input tensor node @p input, starting at the
+ * specified @p offset and extending for the given @p size along each dimension.
  *
  * @param rec The computation graph to which the node will be added.
  * @param input Handle of the input node.
@@ -105,7 +105,7 @@ Node slice(Graph &rec, Node input, std::initializer_list<int> size,
  * @brief Adds a unary sum node to the computation graph.
  * @ingroup unary_operators
  *
- * Computes the sum of all elements in the input tensor node `A`,
+ * Computes the sum of all elements in the input tensor node @p input,
  * producing a scalar tensor node containing the total sum.
  *
  * @param rec The computation graph to which the node will be added.
@@ -120,14 +120,15 @@ Node sum(Graph &rec, Node input);
  * specified dimension.
  * @ingroup unary_operators
  *
- * Computes the sum of elements in the input tensor node `A` along the given
- * dimension `dim`. The resulting tensor shape depends on the `keep_rank` flag:
- * - If `keep_rank` is false (default), the dimension `dim` is removed from the
- * output shape.
- * - If `keep_rank` is true, the dimension `dim` is retained with size 1.
+ * Computes the sum of elements in the input tensor node @p input along the
+ * given dimension @p dim. The resulting tensor shape depends on the @p
+ * keep_rank flag:
+ * - If @p keep_rank is false (default), the dimension @p dim is removed from
+ * the output shape.
+ * - If @p keep_rank is true, the dimension @p dim is retained with size 1.
  *
  * @param rec The computation graph to which the node will be added.
- * @param node Handle of the input node.
+ * @param input Handle of the input node.
  * @param dim The dimension along which to sum.
  * @param keep_rank If true, retains the summed dimension with size 1; if false,
  * removes it.
@@ -140,8 +141,8 @@ Node sum(Graph &rec, Node input, int dim, bool keep_rank = false);
  * @brief Adds a unary mean node to the computation graph.
  * @ingroup unary_operators
  *
- * Computes the mean (average) of all elements in the input tensor node `input`,
- * producing a scalar tensor node containing the total average.
+ * Computes the mean (average) of all elements in the input tensor node @p
+ * input, producing a scalar tensor node containing the total average.
  *
  * @param rec The computation graph to which the node will be added.
  * @param input Handle of the input node.
@@ -155,12 +156,12 @@ Node mean(Graph &rec, Node input);
  * a specified dimension.
  * @ingroup unary_operators
  *
- * Computes the mean of elements in the input tensor node `input` along the
- * given dimension `dim`. The resulting tensor shape depends on the `keep_rank`
- * flag:
- * - If `keep_rank` is false (default), the dimension `dim` is removed from the
- * output shape.
- * - If `keep_rank` is true, the dimension `dim` is retained with size 1.
+ * Computes the mean of elements in the input tensor node @p input along the
+ * given dimension @p dim. The resulting tensor shape depends on the @p
+ * keep_rank flag:
+ * - If @p keep_rank is false (default), the dimension @p dim is removed from
+ * the output shape.
+ * - If @p keep_rank is true, the dimension @p dim is retained with size 1.
  *
  * @param rec The computation graph to which the node will be added.
  * @param input Handle of the input node.
@@ -176,8 +177,8 @@ Node mean(Graph &rec, Node input, int dim, bool keep_rank = false);
  * @brief Adds a unary transpose node to the computation graph.
  * @ingroup unary_operators
  *
- * Transposes the input tensor node `A` according to the permutation `perm`.
- * If `perm` is empty, the tensor is fully transposed by reversing its
+ * Transposes the input tensor node @p input according to the permutation @p
+ * perm. If @p perm is empty, the tensor is fully transposed by reversing its
  * dimensions.
  *
  * @param rec The computation graph to which the node will be added.
@@ -186,7 +187,7 @@ Node mean(Graph &rec, Node input, int dim, bool keep_rank = false);
  *             If not provided or empty, a full transpose (reverse of all axes)
  * is performed.
  * @return A handle of the new node representing the transposed tensor,
- *         with shape adjusted according to `perm` or full transpose.
+ *         with shape adjusted according to @p perm or full transpose.
  */
 Node transpose(Graph &rec, Node input, std::initializer_list<int> perm = {});
 
@@ -199,7 +200,7 @@ Node transpose(Graph &rec, Node input, std::initializer_list<int> perm = {});
  * @brief Adds a binary addition node to the computation graph.
  * @ingroup binary_operators
  *
- * Computes the element-wise sum of two input tensor nodes `lhs` and `rhs`.
+ * Computes the element-wise sum of two input tensor nodes @p lhs and @p rhs.
  * Both tensors must have the same shape or be broadcast-compatible.
  *
  * @param rec The computation graph to which the node will be added.
@@ -214,8 +215,8 @@ Node add(Graph &rec, Node lhs, Node rhs);
  * @brief Adds a binary subtratction node to the computation graph.
  * @ingroup binary_operators
  *
- * Computes the element-wise difference of two input tensor nodes `lhs` and
- * `rhs`. Both tensors must have the same shape or be broadcast-compatible.
+ * Computes the element-wise difference of two input tensor nodes @p lhs and
+ * @p rhs. Both tensors must have the same shape or be broadcast-compatible.
  *
  * @param rec The computation graph to which the node will be added.
  * @param lhs Handle of the first input node.
@@ -230,8 +231,8 @@ Node sub(Graph &rec, Node lhs, Node rhs);
  * graph.
  * @ingroup binary_operators
  *
- * Computes the element-wise product of two input tensor nodes `lhs` and
- * `rhs`. Both tensors must have the same shape or be broadcast-compatible.
+ * Computes the element-wise product of two input tensor nodes @p lhs and
+ * @p rhs. Both tensors must have the same shape or be broadcast-compatible.
  *
  * @param rec The computation graph to which the node will be added.
  * @param lhs Handle of the first input node.
@@ -245,8 +246,8 @@ Node mul(Graph &rec, Node lhs, Node rhs);
  * @brief Adds a binary division node to the computation graph.
  * @ingroup binary_operators
  *
- * Computes the element-wise quotient of two input tensor nodes `lhs` and
- * `rhs`. Both tensors must have the same shape or be broadcast-compatible.
+ * Computes the element-wise quotient of two input tensor nodes @p lhs and
+ * @p rhs. Both tensors must have the same shape or be broadcast-compatible.
  *
  * @param rec The computation graph to which the node will be added.
  * @param lhs Handle of the first input node.
@@ -260,8 +261,8 @@ Node div(Graph &rec, Node lhs, Node rhs);
  * @brief Adds a binary power node to the computation graph.
  * @ingroup binary_operators
  *
- * Computes the element-wise power of two input tensor nodes `lhs` and
- * `rhs`. Both tensors must have the same shape or be broadcast-compatible.
+ * Computes the element-wise power of two input tensor nodes @p lhs and
+ * @p rhs. Both tensors must have the same shape or be broadcast-compatible.
  *
  * @param rec The computation graph to which the node will be added.
  * @param lhs Handle of the first input node.
@@ -275,8 +276,8 @@ Node pow(Graph &rec, Node lhs, Node rhs);
  * @brief Adds a binary minimum node to the computation graph.
  * @ingroup binary_operators
  *
- * Computes the element-wise minimum of two input tensor nodes `lhs` and
- * `rhs`. Both tensors must have the same shape or be broadcast-compatible.
+ * Computes the element-wise minimum of two input tensor nodes @p lhs and
+ * @p rhs. Both tensors must have the same shape or be broadcast-compatible.
  *
  * @param rec The computation graph to which the node will be added.
  * @param lhs Handle of the first input node.
@@ -290,8 +291,8 @@ Node min(Graph &rec, Node lhs, Node rhs);
  * @brief Adds a binary maximum node to the computation graph.
  * @ingroup binary_operators
  *
- * Computes the element-wise maximum of two input tensor nodes `lhs` and
- * `rhs`. Both tensors must have the same shape or be broadcast-compatible.
+ * Computes the element-wise maximum of two input tensor nodes @p lhs and
+ * @p rhs. Both tensors must have the same shape or be broadcast-compatible.
  *
  * @param rec The computation graph to which the node will be added.
  * @param lhs Handle of the first input node.
@@ -305,8 +306,8 @@ Node max(Graph &rec, Node lhs, Node rhs);
  * @brief Adds a binary dot product node to the computation graph.
  * @ingroup binary_operators
  *
- * Computes the element-wise dot product of two input tensor nodes `lhs` and
- * `rhs`. Both tensors must have the same shape or be broadcast-compatible.
+ * Computes the element-wise dot product of two input tensor nodes @p lhs and
+ * @p rhs. Both tensors must have the same shape or be broadcast-compatible.
  *
  * @param rec The computation graph to which the node will be added.
  * @param lhs Handle of the first input node.
@@ -320,8 +321,8 @@ Node dot(Graph &rec, Node lhs, Node rhs);
  * @brief Adds a matrix multiplication node to the computation graph.
  * @ingroup binary_operators
  *
- * Performs matrix multiplication between two input tensor nodes `lhs` and
- * `rhs`. Supports both standard 2D matrix multiplication and batched matrix
+ * Performs matrix multiplication between two input tensor nodes @p lhs and
+ * @p rhs. Supports both standard 2D matrix multiplication and batched matrix
  * multiplication:
  * - If both tensors are rank-2, performs standard matrix multiplication.
  * - If tensors have more than 2 dimensions, performs batched matrix
@@ -341,7 +342,7 @@ Node matmul(Graph &rec, Node lhs, Node rhs);
  * @brief Adds a generalized outer product node to the computation graph.
  * @ingroup binary_operators
  *
- * Computes the outer product of two input tensor nodes `lhs` and `rhs`.
+ * Computes the outer product of two input tensor nodes @p lhs and @p rhs.
  * The result is a tensor whose shape is the concatenation of the shapes of @p
  * lhs and @p rhs. For example:
  * - If @p lhs has shape (m,) and @p rhs has shape (n,), the result has shape
