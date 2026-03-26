d3705fa9af225a5ffaea19173bbf80a0c2551673
FelixBertl <felix.bertl@fh-krems.ac.at>
2026-03-26 16:15:53 +0100

    refactor(operators): simplified transpose

--- src/operators/transpose.cpp
+++ src/operators/transpose.cpp
@@ -2,7 +2,7 @@
 
 #include "../exceptions.hpp"            // for ArgumentError, ShapeError
 #include "../graph/nodes/transp.hpp"    // for NodeTransp
-#include <algorithm>                    // for reverse_copy
+#include <algorithm>                    // for __sort_fn, adjacent_find, sort
 #include <cstddef>                      // for size_t
 #include <initializer_list>             // for initializer_list
 #include <kaad/graph/graph.hpp>         // for Graph, transpose
@@ -11,7 +11,9 @@
 #include <kaad/static_vector.hpp>       // for StaticVector
 #include <kaad/tensor/tensor.hpp>       // for Tensor
 #include <kaad/tensor/tensor_types.hpp> // for Shape, Stride
+#include <kaad/tensor/tensor_view.hpp>  // for TensorViewConst, TensorView
 #include <memory>                       // for unique_ptr, make_unique
+#include <ranges>                       // for __adjacent_find_fn
 #include <span>                         // for span
 #include <string>                       // for basic_string
 #include <utility>                      // for pair
@@ -19,58 +21,50 @@
 
 namespace kaad {
 
+bool contains_duplicates(StaticVector<int> vals) {
+    std::ranges::sort(vals);
+    return std::ranges::adjacent_find(vals) != vals.end();
+}
+
 Node transpose(Graph &rec, Node input, std::initializer_list<int> perm) {
     std::size_t rec_len = rec.nodes.size();
 
     INode *input_ptr = rec.get_node(input);
-    Tensor &input_val = input_ptr->value();
+    TensorViewConst input_val = input_ptr->value().view();
 
     if (input_val.rank() < 2) {
         throw ShapeError(make_graph_errmsg("shape error", rec_len, "transpose",
                                            "A.rank() hast to be > 1",
-                                           {{"A.shape", input_val.shape()}}));
+                                           {{"A.shape", input_val.shape}}));
     }
 
-    Shape shape_t(input_val.rank());
-    Stride stride_t(input_val.rank());
+    Shape shape_buff;
+    Stride stride_buff;
+    TensorViewConst value_t;
     if (perm.size() == 0) {
 
-        std::reverse_copy(input_val.shape().begin(), input_val.shape().end(),
-                          shape_t.data());
-        std::reverse_copy(input_val.stride().begin(), input_val.stride().end(),
-                          stride_t.data());
+        value_t = input_val.transpose(shape_buff, stride_buff);
 
     } else {
         if (perm.size() != input_val.rank()) {
             throw ArgumentError(
-                make_graph_errmsg("argument erro", rec_len, "transpose",
+                make_graph_errmsg("argument error ", rec_len, "transpose",
                                   "perm.size() has to be same as A.rank()",
-                                  {{"A.shape", input_val.shape()}}));
+                                  {{"A.shape", input_val.shape}}));
         }
 
-        StaticVector<int> count(input_val.rank());
-
-        auto *shape_it = shape_t.begin();
-        auto *stride_it = stride_t.begin();
-        for (int idx : perm) {
-
-            count[idx]++;
-
-            *(shape_it++) = input_val.shape()[idx];
-            *(stride_it++) = input_val.stride()[idx];
+        if (contains_duplicates(StaticVector<int>(perm.begin(), perm.end()))) {
+            throw ArgumentError(make_graph_errmsg(
+                "argument error", rec_len, "transpose",
+                "perm has to contain index of every dimension exactly once",
+                {{"perm", perm}}));
         }
-        for (int count_elem : count) {
-            if (count_elem != 1) {
-                throw ArgumentError(make_graph_errmsg(
-                    "argument error", rec_len, "transpose",
-                    "perm has to contain index of every dimension exactly once",
-                    {{"perm", perm}}));
-            }
-        }
+
+        value_t = input_val.transpose(shape_buff, stride_buff, perm);
     }
 
     rec.nodes.push_back(
-        std::make_unique<NodeTransp>(input_ptr, shape_t, stride_t));
+        std::make_unique<NodeTransp>(input_ptr, value_t.shape, value_t.stride));
 
     return rec.back_handle();
 }
