bb5a332d8ca39ce815f4c5ff130e07310e5ed0e7
FelixBertl <felix.bertl@fh-krems.ac.at>
2026-03-31 10:04:39 +0200

    feat(graph): Node::shape

--- include/kaad/graph/node_handle.hpp
+++ include/kaad/graph/node_handle.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
-#include <cstdint>         // for uint32_t
-#include <iostream>        // for ostream
-#include <kaad/scalar.hpp> // for Scalar
-#include <span>            // for span
+#include <cstdint>                // for uint32_t
+#include <iostream>               // for ostream
+#include <kaad/scalar.hpp>        // for Scalar
+#include <kaad/tensor/tensor.hpp> // for operator<<, Tensor
+#include <span>                   // for span
 
 namespace kaad {
 
@@ -48,6 +49,11 @@ class Node {
      * @return Const pointer to the node.
      */
     [[nodiscard]] const INode *node() const;
+
+    /**
+     * @return View of the shape of @c this->value().
+     */
+    [[nodiscard]] ShapeView shape() const;
 
     /**
      * @brief Get the element array of the value tensor.
--- src/graph/node_handle.cpp
+++ src/graph/node_handle.cpp
@@ -19,6 +19,8 @@ namespace kaad {
     return this->origin_->nodes[this->idx()].get();
 }
 
+[[nodiscard]] ShapeView Node::shape() const { return this->value().shape(); }
+
 std::span<Scalar> Node::value_elements() {
     assert(this->node()->is_input());
 
