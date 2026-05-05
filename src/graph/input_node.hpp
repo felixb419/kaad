b57edc6ecd767b4de7584f06754fe9f299f28856
FelixBertl <felix.bertl@fh-krems.ac.at>
2026-05-05 10:01:56 +0200

    refactor(graph): reworked input node

---
+++ src/graph/input_node.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <kaad/graph/inode.hpp>
+
+namespace kaad {
+
+class InputNode : public INode {
+  public:
+    InputNode(ShapeView shape) : INode(shape) {}
+
+    [[nodiscard]] const char *operation_name() const noexcept override {
+        return "input";
+    }
+
+    [[nodiscard]] bool is_evaluated() const noexcept override { return true; }
+
+    [[nodiscard]] bool is_input() const noexcept override { return true; }
+
+    void evaluate() noexcept override {}
+
+    void acc_input_gradients() noexcept override {}
+
+    void reset() override {
+        std::ranges::fill(this->gradient_.elements(), Tensor::value_type{});
+    }
+};
+
+} // namespace kaad
